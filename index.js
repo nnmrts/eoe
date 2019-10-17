b8f539079056341e7caf619519317bde82ba2550
nnmrts <nanomiratus@gmail.com>
2019-10-17 20:36:50 +0200

    removed wrong export and added square for testing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // @flow
 
-export { default as Position } from "./concepts/position.js";
 export { default as ZeroDimensionalSpace } from "./spaces/zero.js";
 export { default as OneDimensionalSpace } from "./spaces/one.js";
 export { default as TwoDimensionalSpace } from "./spaces/two.js";
 export { default as ThreeDimensionalSpace } from "./spaces/three.js";
+export { default as Square } from "./spaces/two/square.js";
