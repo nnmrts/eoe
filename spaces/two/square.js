30e734f89fee867c76bb56357c804970fa1b4ebc
nnmrts <nanomiratus@gmail.com>
2019-10-17 20:37:59 +0200

    morph morph morph
    (probably doesn't work at all)

--- spaces/two/square.js
+++ spaces/two/square.js
@@ -1,10 +1,11 @@
 // @flow
 import Point from "../zero/point.js";
 import GeometricObject from "../../concepts/geometric-object.js";
+import Vector from "../one/vector.js";
+import LineSegment from "../one/line-segment.js";
 
 import Rectangle from "./rectangle.js";
 import Circle from "./circle.js";
-import Vector from "../one/vector.js";
 
 type HelpingPoints = {|
 	vertex1: Point,
@@ -12,6 +13,8 @@ type HelpingPoints = {|
 		vertex2?: Point
 	|} | {|
 		vertex3?: Point
+	|} | {|
+		vertex4?: Point
 	|}
 |};
 
@@ -21,6 +24,21 @@ type RequiredPoints = {|
 		vertex2: Point
 	|} | {|
 		vertex3: Point
+	|} | {|
+		vertex4: Point
+	|} | {|
+		vertex2: Point,
+		vertex3: Point
+	|} | {|
+		vertex2: Point,
+		vertex4: Point
+	|} | {|
+		vertex3: Point,
+		vertex4: Point
+	|} | {|
+		vertex2: Point,
+		vertex3: Point,
+		vertex4: Point
 	|}
 |};
 
@@ -53,11 +71,13 @@ class Square extends Rectangle {
 
 	+tangential = true;
 
+	vertexMap: Map<"A" | "B" | "C" | "D", Point>;
+
 	/**
 	 * creates an instance of Square
 	 * @author nnmrts <nanomiratus@gmail.com>
 	 * @date 2019-09-21
-	 * @param {SquareOptions} [options={sideLength: 1}]
+	 * @param {SquareOptions} [options={edgeLength: 1}]
 	 * side length
 	 * @param {Position} [position=new Position(0, 0)]
 	 * position
@@ -67,6 +87,10 @@ class Square extends Rectangle {
 		edgeLength: 1,
 		vertex1: new Point(0, 0)
 	}) {
+		const {
+			vertex1
+		} = options;
+
 		let edgeLength;
 
 		if (options.edgeLength) {
@@ -81,29 +105,53 @@ class Square extends Rectangle {
 		else if (options.area) {
 			edgeLength = Math.sqrt(options.area);
 		}
-		else if (options.vertex2) {
-			const {
-				vertex1,
-				vertex2
-			} = options;
+		else if (options.vertex2 || options.vertex4) {
+			let secondVertex;
 
-			edgeLength = Math.sqrt(((vertex2.x - vertex1.x) ** 2) + ((vertex2.y - vertex1.y) ** 2));
+			if (options.vertex2) {
+				secondVertex = options.vertex2;
+			}
+			else if (options.vertex4) {
+				secondVertex = options.vertex4;
+			}
+
+			if (secondVertex) {
+				edgeLength = new LineSegment(vertex1, secondVertex).length();
+			}
 		}
 		else if (options.vertex3) {
 			const {
-				vertex1,
 				vertex3
 			} = options;
 
-			edgeLength = Math.sqrt(((vertex3.x - vertex1.x) ** 2) + ((vertex3.y - vertex1.y) ** 2)) / Math.sqrt(2);
+			edgeLength = new LineSegment(vertex1, vertex3).length() / Math.sqrt(2);
 		}
 
 		if (edgeLength) {
-			const {
-				vertex1
-			} = options;
+			let vertex2;
+			let vertex3;
+			let vertex4;
 
-			const vertex2 = options.vertex2 || options.vertex3 ? new Vector(new Vector(vertex1, options.vertex3).midpoint(), options.vertex3)
+			if (options.vertex2) {
+				vertex2 = options.vertex2;
+				vertex3 = options.vertex3 || new Vector(vertex2, vertex1).rotate(90).head;
+				vertex4 = options.vertex4 || new Vector(vertex3, vertex2).rotate(90).head;
+			}
+			else if (options.vertex3) {
+				vertex3 = options.vertex3;
+				vertex2 = new Vector(new Vector(vertex1, vertex3).midpoint(), vertex3).rotate(-90).head;
+				vertex4 = options.vertex4 || new Vector(vertex3, vertex2).rotate(90).head;
+			}
+			else if (options.vertex4) {
+				vertex4 = options.vertex4;
+				vertex3 = new Vector(vertex4, vertex1).rotate(-90).head;
+				vertex2 = new Vector(new Vector(vertex1, vertex3).midpoint(), vertex3).rotate(-90).head;
+			}
+			else {
+				vertex2 = new Point(vertex1.x + edgeLength, vertex1.y);
+				vertex3 = new Point(vertex1.x + edgeLength, vertex1.y - edgeLength);
+				vertex4 = new Point(vertex1.x, vertex1.y - edgeLength);
+			}
 
 			super({
 				edge1Length: edgeLength,
@@ -111,18 +159,43 @@ class Square extends Rectangle {
 				vertex1
 			});
 
-			this.sideLength = edgeLength;
+			this.edgeLength = edgeLength;
 
 			this.vertexMap = new Map();
 
 			this.vertexMap.set("A", vertex1);
-			this.vertexMap.set("B", vertex2 || new Point(1, 0));
-			this.vertexMap.set("C", new Point(position.x, position.y + edgeLength));
-			this.vertexMap.set("D", new Point(position.x, position.y + edgeLength));
+			this.vertexMap.set("B", vertex2);
+			this.vertexMap.set("C", vertex3);
+			this.vertexMap.set("D", vertex4);
 		}
 		else {
 			throw new Error("invalid");
 		}
+	}
+
+	/**
+	 *
+	 *
+	 * @returns {Point}
+	 * midpoint
+	 */
+	midpoint = (): Point => {
+		const {
+			vertexMap: {
+				get: getVertex
+			}
+		} = this;
+
+		const vertex1 = getVertex("A");
+		const vertex2 = getVertex("B");
+
+		if (vertex1 && vertex2) {
+			return new Vector(
+				vertex1,
+				vertex2
+			).midpoint();
+		}
+		throw new Error("midpoint fail");
 	}
 
 	/**
@@ -141,36 +214,120 @@ class Square extends Rectangle {
 	/**
 	 *
 	 *
+	 * @param {number} angle
+	 * angle in degrees
+	 * @param {boolean} clockwise
+	 * clockwise or counterclockwise rotation
+	 * @returns {Square}
+	 * itself
+	 */
+	rotate = (angle: number, clockwise: boolean): Square => {
+		const {
+			vertexMap: {
+				get: getVertex,
+				set: setVertex
+			}
+		} = this;
+
+		const vertex1 = getVertex("A");
+		const vertex2 = getVertex("B");
+		const vertex3 = getVertex("C");
+		const vertex4 = getVertex("D");
+
+		if (vertex1 && vertex2 && vertex3 && vertex4) {
+			setVertex("A", new Vector(this.midpoint(), vertex1).rotate(clockwise ? -angle : angle).head);
+			setVertex("B", new Vector(this.midpoint(), vertex2).rotate(clockwise ? -angle : angle).head);
+			setVertex("C", new Vector(this.midpoint(), vertex3).rotate(clockwise ? -angle : angle).head);
+			setVertex("D", new Vector(this.midpoint(), vertex4).rotate(clockwise ? -angle : angle).head);
+		}
+
+		return this;
+	}
+
+	/**
+	 *
+	 *
 	 * @param {Circle} object
 	 * a geometric object
 	 * @returns {function}
 	 * function
 	 */
-	morph = (object: Circle): function => {
-		if (object instanceof Circle) {
-			const {
-				edgeLength
-			} = this;
+	morph = (object: Circle): (point: Point) => Point => {
+		const {
+			vertexMap: {
+				get: getVertex
+			}
+		} = this;
 
-			return (position: Point): Point => {
+		const vertex1 = getVertex("A");
+		const vertex2 = getVertex("B");
+		const vertex3 = getVertex("C");
+		const vertex4 = getVertex("D");
+
+		if (vertex1 && vertex2 && vertex3 && vertex4) {
+			const edgeAB = new Vector(vertex1, vertex2);
+			const xAxisParallelThroughA = new Vector(vertex1, new Point(vertex1.x + 10, vertex1.y));
+
+			const angle = Math.acos(
+				(
+					(xAxisParallelThroughA.head.x * xAxisParallelThroughA.head.y) +
+					(edgeAB.head.x * edgeAB.head.y)
+				) /
+				(
+					xAxisParallelThroughA.length() *
+					edgeAB.length()
+				)
+			);
+
+			const orthogonalSquare = new Square({
+				vertex1,
+				vertex2,
+				vertex3,
+				vertex4,
+			}).rotate(45 - angle, true);
+
+			if (object instanceof Circle) {
+				const normalMin = -1;
+				const normalMax = 1;
+
 				const {
-					x,
-					y
-				} = position;
+					vertexMap: {
+						get: orthogonalGetVertex
+					}
+				} = orthogonalSquare;
 
-				const normalizedX = (((x - 0) * (1 - -1)) / (edgeLength - 0)) + -1;
-				const normalizedY = (((y - 0) * (1 - -1)) / (edgeLength - 0)) + -1;
+				const orthogonalVertex1 = orthogonalGetVertex("A");
+				const orthogonalVertex2 = orthogonalGetVertex("B");
+				const orthogonalVertex3 = orthogonalGetVertex("C");
 
-				const newX = normalizedX * Math.sqrt(1 - (((normalizedY) ** 2) / 2));
-				const newY = normalizedY * Math.sqrt(1 - (((normalizedX) ** 2) / 2));
+				if (orthogonalVertex1 && orthogonalVertex2 && orthogonalVertex3) {
+					const squareXMin = orthogonalVertex1.x;
+					const squareXMax = orthogonalVertex2.x;
+					const squareYMin = orthogonalVertex1.y;
+					const squareYMax = orthogonalVertex3.y;
 
-				const denormalizedX = (((newX - -1) * (edgeLength - 0)) / (1 - -1)) + 0;
-				const denormalizedY = (((newY - -1) * (edgeLength - 0)) / (1 - -1)) + 0;
+					return (point: Point): Point => {
+						const {
+							x,
+							y
+						} = point;
 
-				return new Point(denormalizedX, denormalizedY);
-			};
+						const normalizedX = (((x - squareXMin) * (normalMax - normalMin)) / (squareXMax - squareXMin)) + normalMin;
+						const normalizedY = (((y - squareYMin) * (normalMax - normalMin)) / (squareYMax - squareYMin)) + normalMin;
+
+						const newX = normalizedX * Math.sqrt(1 - (((normalizedY) ** 2) / 2));
+						const newY = normalizedY * Math.sqrt(1 - (((normalizedX) ** 2) / 2));
+
+						const denormalizedX = (((newX - normalMin) * (squareXMax - squareXMin)) / (normalMax - normalMin)) + squareXMin;
+						const denormalizedY = (((newY - normalMin) * (squareYMax - squareYMin)) / (normalMax - normalMin)) + squareYMin;
+
+						return new Vector(this.midpoint(), new Point(denormalizedX, denormalizedY)).rotate(angle).head;
+					};
+				}
+			}
 		}
-		return () => {};
+
+		return (): Point => new Point(0, 0);
 	}
 }
 
