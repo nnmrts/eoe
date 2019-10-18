fc814c492b71ffd10fd0535ea3665d0fecd88c31
nnmrts <nanomiratus@gmail.com>
2019-10-18 08:00:52 +0200

    added Vector to exports

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ export { default as OneDimensionalSpace } from "./spaces/one.js";
 export { default as TwoDimensionalSpace } from "./spaces/two.js";
 export { default as ThreeDimensionalSpace } from "./spaces/three.js";
 export { default as Point } from "./spaces/zero/point.js";
+export { default as Vector } from "./spaces/one/vector.js";
 export { default as Square } from "./spaces/two/square.js";
 export { default as Circle } from "./spaces/two/circle.js";
